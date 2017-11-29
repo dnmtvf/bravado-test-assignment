c1dc956a1bde550886038121f0c5932d65d3887f
Daniil Matveev <dnmtvf@gmail.com>
2017-11-29 15:50:06 +0300

    reducers cleanup, fix eslint errors

--- app/javascript/reducers.js
+++ app/javascript/reducers.js
@@ -1,43 +1,25 @@
-import { combineReducers } from 'redux'
 import {
   UPDATE_PROFILES_TABLE,
   REQUEST_PROFILES,
   RECEIVE_PROFILES,
-  SELECT_CARD
-} from './actions'
+  SELECT_CARD,
+} from './actions';
 
-
-// address
-// avatar
-// city
-// email
-// name
-// title
-
-const filterProfiles = (state = [], filterQuery, excludeInSearch) => {
-  const filteredProfiles = state.filter(
-    item => {
-      const profileCredentials = Object.entries(item);
-      const filteredCredentials = profileCredentials.filter(i => {
-        const key = i[0];
-        
-        if(excludeInSearch.includes(key)) {
-            return false;
-        }
-        
-        
-        const value = i[1].toLowerCase();
-        return value.includes(filterQuery.toLowerCase());
-        }
-      )
-        return filteredCredentials.length >= 1;
-    }
-  )
-  
-  // return filteredProfiles.filter((item, index) => index <= 100);
-  return filteredProfiles;
-}
-
+const filterProfiles = (state = [], searchQuery, excludeInSearch) => {
+  const findedProfiles = state.filter((card) => {
+    const profileCard = Object.entries(card);
+    const isMatched = profileCard.filter((userInfo) => {
+      const key = userInfo[0];
+      if (excludeInSearch.includes(key)) {
+        return false;
+      }
+      const value = userInfo[1].toLowerCase();
+      return value.includes(searchQuery.toLowerCase());
+    });
+    return isMatched.length > 0;
+  });
+  return findedProfiles;
+};
 
 const rootReducer = (
   state = {
@@ -48,50 +30,52 @@ const rootReducer = (
     profiles: [],
     currentSearchResult: [],
     selectedCards: [],
-    excludeInSearch: ['avatar', 'id']
+    excludeInSearch: ['avatar', 'id'],
   },
-  action
+  action,
 ) => {
   switch (action.type) {
     case REQUEST_PROFILES:
       return {
         ...state,
-        fetchStart: action.fetchStart
-      }
-    case RECEIVE_PROFILES:
-        const profilesWithId = action.profiles.map((item, index) => {
-          return {...item, id: index}
-        })
+        fetchStart: action.fetchStart,
+      };
+    case RECEIVE_PROFILES: {
+      const profilesWithId = action.profiles.map((item, index) => {
         return {
-          ...state,
-          isFetching: false,
-          fetchEnd: action.fetchEnd,
-          // profiles: action.profiles,
-          profiles: profilesWithId,
-          currentSearchResult: profilesWithId
-        }
-      case UPDATE_PROFILES_TABLE:
-        return {
-          ...state,
-          searchQuery: action.searchQuery,
-          currentSearchResult: filterProfiles(state.profiles, action.searchQuery, state.excludeInSearch)
-        }
-      case SELECT_CARD:
-        const isSelected = state.selectedCards.includes(state.profiles[action.cardId]);
-        return {
-          ...state,
-          selectedCards: isSelected ? state.selectedCards.filter(i => i.id != action.cardId)
-          : state.selectedCards.concat(state.profiles[action.cardId]) 
-        }
-      default:
-        return state    
+          ...item,
+          id: index,
+        };
+      });
+      return {
+        ...state,
+        isFetching: false,
+        fetchEnd: action.fetchEnd,
+        profiles: profilesWithId,
+        currentSearchResult: profilesWithId,
+      };
+    }
+    case UPDATE_PROFILES_TABLE:
+      return {
+        ...state,
+        searchQuery: action.searchQuery,
+        currentSearchResult: filterProfiles(
+          state.profiles,
+          action.searchQuery,
+          state.excludeInSearch,
+        ),
+      };
+    case SELECT_CARD: {
+      const isSelected = state.selectedCards.includes(state.profiles[action.cardId]);
+      return {
+        ...state,
+        selectedCards: isSelected ? state.selectedCards.filter(i => i.id !== action.cardId)
+          : state.selectedCards.concat(state.profiles[action.cardId]),
+      };
+    }
+    default:
+      return state;
   }
-}
-
-// const rootReducer = combineReducers({
-//   fetchProfiles,
-  // receiveProfiles
-// })
+};
 
 export default rootReducer;
-// export default fetchProfiles;
