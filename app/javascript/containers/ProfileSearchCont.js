cab460fe6eae3a1baf5cf4806963145237bcc7d5
Daniil Matveev <dnmtvf@gmail.com>
2017-11-29 15:52:15 +0300

    ProfileSearchContainer cleanup

--- app/javascript/containers/ProfileSearchCont.js
+++ app/javascript/containers/ProfileSearchCont.js
@@ -1,32 +1,27 @@
 import { connect } from 'react-redux';
-
 import ProfileSearch from '../components/profileSearch/profileSearch';
 import { selectCard } from '../actions';
-
-// .filter(
-//   (item, index) => index <= 100
-// ),
 
 const mapStateToProps = (state) => {
   return {
     currentSearchResults: state.currentSearchResult,
     isFetching: state.isFetching,
     searchQuery: state.searchQuery,
-    selectedCards: state.selectedCards
-  }
-}
+    selectedCards: state.selectedCards,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onClickSelectBtn: cardId => {
-      dispatch(selectCard(cardId))
-    }
-  }
-}
+    onClickSelectBtn: (cardId) => {
+      dispatch(selectCard(cardId));
+    },
+  };
+};
 
 const ProfileSearchCont = connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(ProfileSearch);
 
 export default ProfileSearchCont;
