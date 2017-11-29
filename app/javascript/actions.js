cc9a488e560345bb5ca14131a6ad3457cea372e0
Daniil Matveev <dnmtvf@gmail.com>
2017-11-29 13:07:50 +0300

    Cleanup actions.js, fix eslint errors

--- app/javascript/actions.js
+++ app/javascript/actions.js
@@ -1,49 +1,45 @@
 import fetch from 'cross-fetch';
 
-export const UPDATE_PROFILES_TABLE = 'UPDATE_PROFILES_TABLE'
+export const UPDATE_PROFILES_TABLE = 'UPDATE_PROFILES_TABLE';
 
 export const updateProfilesTable = (searchQuery) => {
   return {
     type: UPDATE_PROFILES_TABLE,
-    searchQuery
-  }
+    searchQuery,
+  };
 };
 
-export const REQUEST_PROFILES = 'REQUEST_PROFILES'
+export const REQUEST_PROFILES = 'REQUEST_PROFILES';
+
 const requestProfiles = () => {
   return {
     type: REQUEST_PROFILES,
-    fetchStart: Date.now()
-  }
-}
+    fetchStart: Date.now(),
+  };
+};
 
-export const RECEIVE_PROFILES = 'RECEIVE_PROFILES'
-function receiveProfiles(json) {
+export const RECEIVE_PROFILES = 'RECEIVE_PROFILES';
+
+const receiveProfiles = (json) => {
   return {
     type: RECEIVE_PROFILES,
     profiles: json,
-    fetchEnd: Date.now()
-  }
-}
+    fetchEnd: Date.now(),
+  };
+};
 
-export const SELECT_CARD = 'SELECT_CARD'
+export const SELECT_CARD = 'SELECT_CARD';
+
 export const selectCard = (cardId) => {
   return {
     type: SELECT_CARD,
-    cardId
-  }
-}
+    cardId,
+  };
+};
 
-export const fetchProfiles = () => {
-  return (dispatch) => {
-    dispatch(requestProfiles())
-    return fetch(`/users`)
-      .then(
-        response => response.json(),
-        error => console.log('An error occurred.', error)
-      )
-      .then(json =>
-        dispatch(receiveProfiles(json))
-      )
-  }
-}
+export const fetchProfiles = () => (dispatch) => {
+  dispatch(requestProfiles());
+  return fetch('/users')
+    .then(response => response.json())
+    .then(json => dispatch(receiveProfiles(json)));
+};
