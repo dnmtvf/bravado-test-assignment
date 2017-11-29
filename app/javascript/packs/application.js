698e5b0d69bbf1908e0a6cf8371b611f4c4980a3
Daniil Matveev <dnmtvf@gmail.com>
2017-11-29 12:20:50 +0300

    application.js cleanup, fix eslint errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,55 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-
-import { fetchProfiles, updateProfilesTable } from '../actions'
-import rootReducer from '../reducers'
-
-// import ProfileSearch from '../components/profileSearch/profileSearch';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { fetchProfiles } from '../actions';
+import rootReducer from '../reducers';
 import ProfileSearchCont from '../containers/ProfileSearchCont';
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
 
 const store = createStore(
   rootReducer,
   applyMiddleware(
-    thunkMiddleware, // lets us dispatch() functions
-    loggerMiddleware // neat middleware that logs actions
-  )
-)
+    thunkMiddleware,
+    loggerMiddleware,
+  ),
+);
 
-store
-  .dispatch(fetchProfiles())
-  // .then(() => console.log(store.getState()))
+store.dispatch(fetchProfiles());
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-// ReactDOM.render(
-//       <Provider store={store}>
-//         <ProSearchCont />
-//       </Provider>,
-//       document.getElementById('root')
-//     )
-// })
-
+const render = (App) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
 
 const devAppWrapper = (App) => {
-  document.addEventListener('DOMContentLoaded', () => {  
-    ReactDOM.render(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-      document.getElementById('root')
-    )
-  })
-} 
+  document.addEventListener('DOMContentLoaded', () => {
+    render(App);
+  });
+};
 
-if(process.env.NODE_ENV === 'development') {
-  // devAppWrapper(ProfileSearch);
+if (process.env.NODE_ENV === 'development') {
   devAppWrapper(ProfileSearchCont);
 } else {
   render(ProfileSearchCont);
