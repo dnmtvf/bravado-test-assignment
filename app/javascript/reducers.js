245681e4ae6e9b1ee6728a0aa5f238aac49356eb
Daniil Matveev <dnmtvf@gmail.com>
2017-12-10 13:37:32 +0300

    Refactor reducers, add search cache

--- app/javascript/reducers.js
+++ app/javascript/reducers.js
@@ -3,34 +3,46 @@ import {
   REQUEST_PROFILES,
   RECEIVE_PROFILES,
   SELECT_CARD,
+  SHOW_MORE,
 } from './actions';
 
-const filterProfiles = (state = [], searchQuery, excludeInSearch) => {
-  const findedProfiles = state.filter((card) => {
-    const profileCard = Object.entries(card);
-    const isMatched = profileCard.filter((userInfo) => {
-      const key = userInfo[0];
-      if (excludeInSearch.includes(key)) {
+const filterArrBySubstr = (arr = [], subStr, excludedProps, limit) => {
+  if (subStr === '') {
+    return arr.filter((item, index) => index < limit);
+  }
+
+  const filteredItemsCount = arr.filter((i, index) => {
+    const curObj = Object.entries(i);
+    const isMatchedCount = curObj.filter((ii) => {
+      const propName = ii[0];
+      if (excludedProps.includes(propName)) {
         return false;
       }
-      const value = userInfo[1].toLowerCase();
-      return value.includes(searchQuery.toLowerCase());
+      const propValue = ii[1].toLowerCase();
+      return propValue.includes(subStr.toLowerCase());
     });
-    return isMatched.length > 0;
+    return isMatchedCount.length > 0 && index < limit;
   });
-  return findedProfiles;
+
+  // return filteredItemsCount.filter((item, index) => index < limit);
+  return filteredItemsCount;
 };
 
 const rootReducer = (
   state = {
-    searchQuery: '',
-    isFetching: true,
-    fetchStart: null,
-    fetchEnd: null,
+    isFetching: false,
+    cacheLimit: 100,
+    initCacheLimit: 100,
     profiles: [],
-    currentSearchResult: [],
+    initProfiles: [],
+    searchQuery: '',
+    searchResult: [],
+    showPerPage: 25,
+    showedCards: 25,
+    isRestToShow: false,
+    searchCache: {},
     selectedCards: [],
-    excludeInSearch: ['avatar', 'id'],
+    excludedInSearch: ['avatar', 'id'],
   },
   action,
 ) => {
@@ -38,41 +50,98 @@ const rootReducer = (
     case REQUEST_PROFILES:
       return {
         ...state,
-        fetchStart: action.fetchStart,
+        isFetching: true,
       };
     case RECEIVE_PROFILES: {
-      const profilesWithId = action.profiles.map((item, index) => {
-        return {
-          ...item,
-          id: index,
-        };
-      });
+      const initialProfilesList = action.profiles.filter((item, index) => index < state.cacheLimit);
       return {
         ...state,
         isFetching: false,
-        fetchEnd: action.fetchEnd,
-        profiles: profilesWithId,
-        currentSearchResult: profilesWithId,
+        profiles: action.profiles,
+        searchResult: initialProfilesList,
+        initProfiles: initialProfilesList,
+        isRestToShow: initialProfilesList.length > state.showPerPage,
       };
     }
-    case UPDATE_PROFILES_TABLE:
+    case UPDATE_PROFILES_TABLE: {
+      if (state.searchCache[action.searchQuery] !== undefined) {
+        return {
+          ...state,
+          searchQuery: action.searchQuery,
+          searchResult: state.searchCache[action.searchQuery],
+          showedCards: state.showPerPage,
+          cacheLimit: state.initCacheLimit,
+          isRestToShow: state.searchCache[action.searchQuery].length > state.showPerPage,
+        };
+      }
+
+      if(action.searchQuery === '') {
+        return {
+          ...state,
+          searchQuery: action.searchQuery,
+          searchResult: state.initProfiles,
+          showedCards: state.showPerPage,
+          cacheLimit: state.initCacheLimit,
+          isRestToShow: state.initProfiles.length > state.showPerPage,
+        }
+      }
+
+      const filteredProfiles = filterArrBySubstr(
+        state.profiles,
+        action.searchQuery,
+        state.excludedInSearch,
+        state.cacheLimit,
+      );
+
       return {
         ...state,
         searchQuery: action.searchQuery,
-        currentSearchResult: filterProfiles(
-          state.profiles,
-          action.searchQuery,
-          state.excludeInSearch,
-        ),
-      };
-    case SELECT_CARD: {
-      const isSelected = state.selectedCards.includes(state.profiles[action.cardId]);
-      return {
-        ...state,
-        selectedCards: isSelected ? state.selectedCards.filter(i => i.id !== action.cardId)
-          : state.selectedCards.concat(state.profiles[action.cardId]),
+        searchResult: filteredProfiles,
+        showedCards: state.showPerPage,
+        cacheLimit: state.initCacheLimit,
+        searchCache: {
+          ...state.searchCache,
+          [action.searchQuery]: filteredProfiles,
+        },
+        isRestToShow: filteredProfiles.length > state.showPerPage,
       };
     }
+    case SELECT_CARD: {
+      const isSelected = state.selectedCards.includes(action.cardId);
+      return {
+        ...state,
+        selectedCards: isSelected ? state.selectedCards.filter(i => i !== action.cardId)
+          : state.selectedCards.concat(action.cardId),
+      };
+    }
+    case SHOW_MORE: {
+      const newShowedCards = state.showedCards + state.showPerPage;
+
+      if (newShowedCards > state.cacheLimit) {
+        const newCacheLimit = state.cacheLimit + state.initCacheLimit;
+        const filteredProfiles = filterArrBySubstr(
+          state.profiles,
+          state.searchQuery,
+          state.excludedInSearch,
+          newCacheLimit,
+        );
+
+        return {
+          ...state,
+          cacheLimit: newCacheLimit,
+          searchResult: filteredProfiles,
+          showedCards: newShowedCards,
+          isRestToShow: filteredProfiles.length > state.showPerPage,
+        };
+      }
+
+      return {
+        ...state,
+        showedCards: newShowedCards,
+        isRestToShow: state.searchResult.length > (state.showPerPage + state.showedCards),
+      };
+    }
+
     default:
       return state;
   }
