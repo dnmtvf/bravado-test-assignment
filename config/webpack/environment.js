e264c70762baf5b4f23fdfd9a30c3046e78e4515
Daniil Matveev <dnmtvf@gmail.com>
2017-12-12 12:08:48 +0300

    Update sugarss loader settings

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,8 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
 environment.loaders.set('sss', {
   test: /\.sss/,
-  loader: "style-loader!css-loader!postcss-loader?parser=sugarss"
-})
+  loader: 'style-loader!css-loader!postcss-loader?parser=sugarss',
+});
 
-module.exports = environment
+module.exports = environment;
