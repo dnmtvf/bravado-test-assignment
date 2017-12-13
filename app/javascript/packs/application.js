46ad788132699b563d726d8d7bc300d611640f9c
Daniil Matveev <dnmtvf@gmail.com>
2017-12-13 16:57:57 +0300

    Fix dev env

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,21 +4,30 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { fetchProfiles } from '../actions';
-import rootReducer from '../reducers';
-import ProfileSearchCont from '../containers/ProfileSearchCont';
+import { fetchProfiles } from './actions';
+import rootReducer from './reducers';
+import ProfileSearchCont from './containers/ProfileSearchCont';
+import './bootstrap-reboot.css';
 
-const loggerMiddleware = createLogger();
+const isDev = process.env.NODE_ENV === 'development';
 
+if (isDev) {
+  const loggerMiddleware = createLogger();
+  const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+}
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware,
-  ),
+  applyMiddleware(thunkMiddleware),
 );
+
 
 store.dispatch(fetchProfiles());
 
@@ -37,7 +46,7 @@ const devAppWrapper = (App) => {
   });
 };
 
-if (process.env.NODE_ENV === 'development') {
+if (isDev) {
   devAppWrapper(ProfileSearchCont);
 } else {
   render(ProfileSearchCont);
